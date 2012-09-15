179e0e82184c404854b226efd720a02e8b26c5f4
Richard Knop <richard.knop@saffrondigital.com>
2012-09-15 01:12:02 +0100

    enabled testing actions with doctrine

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,5 +1,30 @@
 <?php
 
-chdir(dirname(__DIR__));
-include __DIR__ . '/../init_autoloader.php';
-Zend\Mvc\Application::init(include 'config/application.config.php');
+use Zend\ServiceManager\ServiceManager;
+use Zend\Mvc\Service\ServiceManagerConfig;
+
+class Bootstrap
+{
+	static $serviceManager;
+	
+	static public function go()
+	{
+		chdir(dirname(__DIR__));
+		include __DIR__ . '/../init_autoloader.php';
+		$config = include 'config/application.config.php';
+		Zend\Mvc\Application::init($config);
+		
+		$serviceManager = new ServiceManager(new ServiceManagerConfig);
+		$serviceManager->setService('ApplicationConfig', $config);
+		$serviceManager->get('ModuleManager')->loadModules();
+
+		self::$serviceManager = $serviceManager;
+	}
+	
+	static public function getServiceManager()
+	{
+		return self::$serviceManager;
+	}
+}
+
+Bootstrap::go();
--- tests/module/Api/src/Api/Controller/ResourceControllerTest.php
+++ tests/module/Api/src/Api/Controller/ResourceControllerTest.php
@@ -20,81 +20,67 @@ class ResourceControllerTest extends PHPUnit_Framework_TestCase
 	{
 		$this->_controller = new ResourceController;
 		$this->_request = new Request;
+		$this->_response = new Response;
 		$this->_routeMatch = new RouteMatch(array('controller' => 'resource'));
 		$this->_event = new MvcEvent();
 		$this->_event->setRouteMatch($this->_routeMatch);
 		$this->_controller->setEvent($this->_event);
+		$this->_controller->setServiceLocator(Bootstrap::getServiceManager());
 	}
 
 	public function testGetListHttpStatusCode()
 	{
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-	
-	public function testGetListContentTypeHeader()
-	{
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals('application/json', $response->getHeaders()->get('Content-Type')->getFieldValue());
+		$this->_controller->dispatch($this->_request, $this->_response);
+		$this->assertEquals(200, $this->_response->getStatusCode());
 	}
 
 	public function testGetListResponseBody()
 	{
-		$response = $this->_controller->dispatch($this->_request);
 		$expected = array(
 			'a' => 'b',
 			'c' => 'd',
 			'e' => 'f',
 		);
-		$this->assertEquals(json_encode($expected), $response->getBody());
+		$this->assertEquals(
+			$expected,
+			$this->_controller->dispatch($this->_request, $this->_response)
+		);
 	}
 	
 	public function testGetHttpStatusCode()
 	{
 		$this->_routeMatch->setParam('id', 123);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-	
-	public function testGetContentTypeHeader()
-	{
-		$this->_routeMatch->setParam('id', 123);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals('application/json', $response->getHeaders()->get('Content-Type')->getFieldValue());
+		$this->_controller->dispatch($this->_request, $this->_response);
+		$this->assertEquals(200, $this->_response->getStatusCode());
 	}
 	
 	public function testGetResponseBody()
 	{
 		$this->_routeMatch->setParam('id', 123);
-		$response = $this->_controller->dispatch($this->_request);
+		$response = $this->_controller->dispatch($this->_request, $this->_response);
 		$expected = array(
 			'id' => 123,
 		);
-		$this->assertEquals(json_encode($expected), $response->getBody());
+		$this->assertEquals($expected, $response);
 	}
 	
 	public function testCreateHttpStatusCode()
 	{
 		$this->_request->setMethod(Request::METHOD_POST);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-	
-	public function testCreateContentTypeHeader()
-	{
-		$this->_request->setMethod(Request::METHOD_POST);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals('application/json', $response->getHeaders()->get('Content-Type')->getFieldValue());
+		$this->_controller->dispatch($this->_request, $this->_response);
+		$this->assertEquals(200, $this->_response->getStatusCode());
 	}
 	
 	public function testCreateResponseBody()
 	{
 		$this->_request->setMethod(Request::METHOD_POST);
-		$response = $this->_controller->dispatch($this->_request);
 		$expected = array(
 			'created'
 		);
-		$this->assertEquals(json_encode($expected), $response->getBody());
+		$this->assertEquals(
+			$expected,
+			$this->_controller->dispatch($this->_request, $this->_response)
+		);
 	}
 	
 	/**
@@ -104,34 +90,28 @@ class ResourceControllerTest extends PHPUnit_Framework_TestCase
 	public function testUpdateThrowsExceptionWhenNoID()
 	{
 		$this->_request->setMethod(Request::METHOD_PUT);
-		$this->_controller->dispatch($this->_request);
+		$this->_controller->dispatch($this->_request, $this->_response);
 	}
 	
 	public function testUpdateHttpStatusCode()
 	{
 		$this->_routeMatch->setParam('id', 123);
 		$this->_request->setMethod(Request::METHOD_PUT);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-	
-	public function testUpdateContentTypeHeader()
-	{
-		$this->_routeMatch->setParam('id', 123);
-		$this->_request->setMethod(Request::METHOD_PUT);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals('application/json', $response->getHeaders()->get('Content-Type')->getFieldValue());
+		$this->_controller->dispatch($this->_request, $this->_response);
+		$this->assertEquals(200, $this->_response->getStatusCode());
 	}
 	
 	public function testUpdateResponseBody()
 	{
 		$this->_routeMatch->setParam('id', 123);
 		$this->_request->setMethod(Request::METHOD_PUT);
-		$response = $this->_controller->dispatch($this->_request);
 		$expected = array(
 			'updated'
 		);
-		$this->assertEquals(json_encode($expected), $response->getBody());
+		$this->assertEquals(
+			$expected,
+			$this->_controller->dispatch($this->_request, $this->_response)
+		);
 	}
 	
 	/**
@@ -141,34 +121,28 @@ class ResourceControllerTest extends PHPUnit_Framework_TestCase
 	public function testDeleteThrowsExceptionWhenNoID()
 	{
 		$this->_request->setMethod(Request::METHOD_DELETE);
-		$this->_controller->dispatch($this->_request);
+		$this->_controller->dispatch($this->_request, $this->_response);
 	}
 	
 	public function testDeleteHttpStatusCode()
 	{
 		$this->_routeMatch->setParam('id', 123);
 		$this->_request->setMethod(Request::METHOD_DELETE);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals(200, $response->getStatusCode());
-	}
-	
-	public function testDeleteContentTypeHeader()
-	{
-		$this->_routeMatch->setParam('id', 123);
-		$this->_request->setMethod(Request::METHOD_DELETE);
-		$response = $this->_controller->dispatch($this->_request);
-		$this->assertEquals('application/json', $response->getHeaders()->get('Content-Type')->getFieldValue());
+		$this->_controller->dispatch($this->_request, $this->_response);
+		$this->assertEquals(200, $this->_response->getStatusCode());
 	}
 	
 	public function testDeleteResponseBody()
 	{
 		$this->_routeMatch->setParam('id', 123);
 		$this->_request->setMethod(Request::METHOD_DELETE);
-		$response = $this->_controller->dispatch($this->_request);
 		$expected = array(
 			'deleted'
 		);
-		$this->assertEquals(json_encode($expected), $response->getBody());
+		$this->assertEquals(
+			$expected,
+			$this->_controller->dispatch($this->_request, $this->_response)
+		);
 	}
 
 }
